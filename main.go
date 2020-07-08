aaf0cb05e711d3a989967f8f03e08a60fb5a4782
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2020-07-08 16:48:20 +0200

    update new label with style

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 	)
@@ -9,7 +10,11 @@ func main()  {
 	a := app.New()
 	w:= a.NewWindow("Hello")
 	w.SetContent(widget.NewVBox(
-		widget.NewLabel("hello k6"),
+		widget.NewLabelWithStyle("hello k6",2,fyne.TextStyle{
+			Bold:      true,
+			Italic:    false,
+			Monospace: true,
+		}),
 		widget.NewButton("Quit", func() {
 			a.Quit()
 		})))
